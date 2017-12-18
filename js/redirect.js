b3376daf6b906e1b5ab21c9fbda2472bb5db0ccb
MahiSharew <34085590+MahiSharew@users.noreply.github.com>
2017-12-18 21:11:29 +0900

    Add files via upload

---
+++ js/redirect.js
@@ -0,0 +1,74 @@
+
+//#################################################################
+// implementation of redirect buttons on the simulation html pages
+//#################################################################
+
+function myRedirectRing(){
+    window.location.href = "./index.html";
+}
+
+function myRedirectOnramp(){
+    window.location.href = "./onramp.html";
+}
+
+function myRedirectOfframp(){
+    window.location.href = "./offramp.html";
+}
+
+function myRedirectRoadworks(){
+    window.location.href = "./roadworks.html";
+}
+
+function myRedirectUphill(){
+    window.location.href = "./uphill.html";
+}
+
+function myRedirectRouting(){
+    window.location.href = "./routing.html";
+}
+
+function myRedirectUphill(){
+    window.location.href = "./uphill.html";
+}
+
+function myRedirectTrafficlight(){
+    window.location.href = "./trafficlight.html";
+}
+
+// for ethi 
+
+
+function myRedirectRingEth(){
+    window.location.href = "./index_eth.html";
+}
+
+function myRedirectOnrampEth(){
+    window.location.href = "./onramp_eth.html";
+}
+
+function myRedirectOfframpEth(){
+    window.location.href = "./offramp_eth.html";
+}
+
+function myRedirectRoadworksEth(){
+    window.location.href = "./roadworks_eth.html";
+}
+
+function myRedirectUphillEth(){
+    window.location.href = "./uphill_eth.html";
+}
+
+function myRedirectRoutingEth(){
+    window.location.href = "./routing_eth.html";
+}
+
+function myRedirectUphillEth(){
+    window.location.href = "./uphill_eth.html";
+}
+
+function myRedirectTrafficlightEth(){
+    window.location.href = "./trafficlight_eth.html";
+}
+
+
+
